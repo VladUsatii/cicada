c7f88769f04efa0c8a2edf1eecb45a29abf01141
Vlad Usatii <vladusatii@gmail.com>
2023-04-18 16:51:35 -0500

    fixed conn threading

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from verack import pack_verack, send_verack
 from networkaddr import unpack_netaddr
 from message_struct import *
 
-def handle_connection(conn, addr, my_nonce, s):
+def handle_connection(conn, addr, my_nonce):
 	print(f"Connection from {addr}")
 	data = conn.recv(1024) # increase to 4096 or 8192 in future
 	did_verack = False # marks whether or not the users have exchanged versions
@@ -15,7 +15,8 @@ def handle_connection(conn, addr, my_nonce, s):
 				vers = unpack_message(data, my_nonce) # returns (verack, True) if successful
 				if vers[1] is True:
 					did_verack = vers[1]
-					send_verack(vers[0], addr, s)
+					conn.sendall(vers[0])
+					#send_verack(vers[0], addr, s)
 			else:
 				msg = unpack_message(data, my_nonce) # returns msg that is ready to send
 				conn.sendall(msg) # Send the msg variable back to the other user
@@ -36,7 +37,7 @@ def main(HOST: str, PORT: int, my_nonce: int, max_connections: int):
 			print(f"Maximum number of connections reached ({max_connections}), rejecting connection from {addr}.")
 			conn.close()
 		else:
-			t = threading.Thread(target=handle_connection, args=(conn, addr, my_nonce, s))
+			t = threading.Thread(target=handle_connection, args=(conn, addr, my_nonce))
 			t.start()
 			threads.append(t)
 
